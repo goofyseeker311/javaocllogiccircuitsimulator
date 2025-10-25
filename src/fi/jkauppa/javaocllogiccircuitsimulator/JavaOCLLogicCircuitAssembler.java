4602fc83d5ae2e1b0cf6be6d9c6a343452b0b921
goofyseeker311 <goofyseeker311@gmail.com>
2025-10-25 06:46:20 +0300

    Update JavaOCLLogicCircuitAssembler.java
    added input file line reading code.

--- src/fi/jkauppa/javaocllogiccircuitsimulator/JavaOCLLogicCircuitAssembler.java
+++ src/fi/jkauppa/javaocllogiccircuitsimulator/JavaOCLLogicCircuitAssembler.java
@@ -1,6 +1,10 @@
 package fi.jkauppa.javaocllogiccircuitsimulator;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class JavaOCLLogicCircuitAssembler {
 
@@ -11,6 +15,18 @@ public class JavaOCLLogicCircuitAssembler {
 			File inputfile = new File(filename);
 			long filesize = inputfile.length();
 			System.out.println("file: "+filename+", size: "+filesize+" bytes");
+			try {
+				BufferedReader filereader = new BufferedReader(new FileReader(inputfile));
+				String readline = null;
+				while((readline=filereader.readLine())!=null) {
+					System.out.println("readline: "+readline);
+				}
+				filereader.close();
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
 		System.out.println("exit.");
 	}
