bb3c214557bc56d0f5deacb08252a4eedec22f7a
goofyseeker311 <goofyseeker311@gmail.com>
2025-10-25 07:11:05 +0300

    Update JavaOCLLogicCircuitAssembler.java
    changed parsing in class structure to run in as instance of the class. added compileline() function to parse each line with a return instruction value.

--- src/fi/jkauppa/javaocllogiccircuitsimulator/JavaOCLLogicCircuitAssembler.java
+++ src/fi/jkauppa/javaocllogiccircuitsimulator/JavaOCLLogicCircuitAssembler.java
@@ -7,27 +7,38 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class JavaOCLLogicCircuitAssembler {
-
 	public static void main(String[] arg) {
 		System.out.println("init.");
-		if (arg.length>0) {
-			String filename = arg[0];
-			File inputfile = new File(filename);
-			long filesize = inputfile.length();
-			System.out.println("file: "+filename+", size: "+filesize+" bytes");
-			try {
-				BufferedReader filereader = new BufferedReader(new FileReader(inputfile));
-				String readline = null;
-				while((readline=filereader.readLine())!=null) {
-					System.out.println("readline: "+readline);
-				}
-				filereader.close();
-			} catch (FileNotFoundException e) {
-				e.printStackTrace();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
+		if (arg.length>1) {
+			String filenamein = arg[0];
+			String filenameout = arg[1];
+			JavaOCLLogicCircuitAssembler compiler = new JavaOCLLogicCircuitAssembler();
+			compiler.parse(filenamein, filenameout);
 		}
 		System.out.println("exit.");
+	}
+	
+	public void parse(String filein, String fileout) {
+		File inputfile = new File(filein);
+		long filesize = inputfile.length();
+		System.out.println("filein: "+filein+", fileout: "+fileout+", size: "+filesize+" bytes");
+		try {
+			BufferedReader filereader = new BufferedReader(new FileReader(inputfile));
+			String readline = null;
+			while((readline=filereader.readLine())!=null) {
+				long insval = compileline(readline);
+				System.out.println("readline: "+readline+", insval: "+insval);
+			}
+			filereader.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public long compileline(String codeline) {
+		long insvalue = 0x0;
+		return insvalue;
 	}
 }
