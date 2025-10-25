1f033a2d26903d4c20842c3d9538dd99ba37119a
goofyseeker311 <goofyseeker311@gmail.com>
2025-10-25 06:25:59 +0300

    added muxrisccoreprogram.asm
    added testing custom risc assembly text file and some initial arguments and file processing code.

--- src/fi/jkauppa/javaocllogiccircuitsimulator/JavaOCLLogicCircuitAssembler.java
+++ src/fi/jkauppa/javaocllogiccircuitsimulator/JavaOCLLogicCircuitAssembler.java
@@ -1,8 +1,17 @@
 package fi.jkauppa.javaocllogiccircuitsimulator;
 
+import java.io.File;
+
 public class JavaOCLLogicCircuitAssembler {
 
 	public static void main(String[] arg) {
-		
+		System.out.println("init.");
+		if (arg.length>0) {
+			String filename = arg[0];
+			File inputfile = new File(filename);
+			long filesize = inputfile.length();
+			System.out.println("file: "+filename+", size: "+filesize+" bytes");
+		}
+		System.out.println("exit.");
 	}
 }
