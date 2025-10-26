a82ae24aad60fbf8ce2745249da2f4e3b1db290f
goofyseeker311 <goofyseeker311@gmail.com>
2025-10-26 06:02:13 +0200

    Create JavaOCLLogicCircuitEmulator.java
    added initial empty code for JavaOCLLogicCircuitEmulator that emulates full 32k core chip.

---
+++ src/fi/jkauppa/javaocllogiccircuitsimulator/JavaOCLLogicCircuitEmulator.java
@@ -0,0 +1,14 @@
+package fi.jkauppa.javaocllogiccircuitsimulator;
+
+public class JavaOCLLogicCircuitEmulator {
+
+	public static void main(String[] arg) {
+		System.out.println("init.");
+		if (arg.length>1) {
+			String filenamein = arg[0];
+			String filenameout = arg[1];
+		}
+		System.out.println("exit.");
+	}
+
+}
