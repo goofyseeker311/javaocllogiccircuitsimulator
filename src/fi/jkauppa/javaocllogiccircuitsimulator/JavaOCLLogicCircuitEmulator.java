41061af6ccad025d29be8626da5ff0abab886da1
goofyseeker311 <goofyseeker311@gmail.com>
2025-10-26 12:55:45 +0200

    Update JavaOCLLogicCircuitEmulator.java
    implemented mem, cmp, add, sub and copy instructions, others only partially implemented, but enough to run the example code working fully.

--- src/fi/jkauppa/javaocllogiccircuitsimulator/JavaOCLLogicCircuitEmulator.java
+++ src/fi/jkauppa/javaocllogiccircuitsimulator/JavaOCLLogicCircuitEmulator.java
@@ -72,6 +72,7 @@ public class JavaOCLLogicCircuitEmulator {
 		private int instructionstep = 0;
 		private int programcounter = 0;
 		private ByteBuffer instbytes = ByteBuffer.allocate(8);
+		private ByteBuffer longbytes = ByteBuffer.allocate(8);
 		
 		public void loadprogram(long[] program) {
 			if (program!=null) {
@@ -110,6 +111,119 @@ public class JavaOCLLogicCircuitEmulator {
 				programcounter = (int)oldregisters[regX];
 			} else if (insT==0x02) {
 				newregisters[regX] = (regY<<16) + regZ;
+				programcounter++;
+			} else if (insT==0x03) {
+				if (instructionstep==1) {
+					newregisters[regX] = memoryram[(int)oldregisters[regY]];
+					instructionstep = 0;
+					programcounter++;
+				} else {
+					instructionstep = 1;
+				}
+			} else if (insT==0x13) {
+				if (instructionstep==1) {
+					memoryram[(int)oldregisters[regY]] = oldregisters[regX];
+					instructionstep = 0;
+					programcounter++;
+				} else {
+					instructionstep = 1;
+				}
+			} else if (insT==0x04) {
+				newregisters[regX] = oldregisters[regX]&(~(0x1<<bitI));
+				if (oldregisters[regY]==0) {
+					newregisters[regX] = newregisters[regX]|(0x1<<bitI);
+				}
+				programcounter++;
+			} else if (insT==0x14) {
+				newregisters[regX] = oldregisters[regX]&(~(0x1<<bitI));
+				if (oldregisters[regY]<0) {
+					newregisters[regX] = newregisters[regX]|(0x1<<bitI);
+				}
+				programcounter++;
+			} else if (insT==0x24) {
+				newregisters[regX] = oldregisters[regX]&(~(0x1<<bitI));
+				longbytes.clear();
+				longbytes.putLong(oldregisters[regY]).rewind();
+				double longdouble = longbytes.asDoubleBuffer().get();
+				if (longdouble==0.0f) {
+					newregisters[regX] = newregisters[regX]|(0x1<<bitI);
+				}
+				programcounter++;
+			} else if (insT==0x34) {
+				newregisters[regX] = oldregisters[regX]&(~(0x1<<bitI));
+				longbytes.clear();
+				longbytes.putLong(oldregisters[regY]).rewind();
+				double longdouble = longbytes.asDoubleBuffer().get();
+				if (longdouble<0.0f) {
+					newregisters[regX] = newregisters[regX]|(0x1<<bitI);
+				}
+				programcounter++;
+			} else if (insT==0x05) {
+				newregisters[regX] = oldregisters[regY] + oldregisters[regZ];
+				programcounter++;
+			} else if (insT==0x15) {
+				programcounter++;
+			} else if (insT==0x25) {
+				newregisters[regX] = oldregisters[regY] - oldregisters[regZ];
+				programcounter++;
+			} else if (insT==0x35) {
+				programcounter++;
+			} else if (insT==0x45) {
+				programcounter++;
+			} else if (insT==0x55) {
+				programcounter++;
+			} else if (insT==0x65) {
+				programcounter++;
+			} else if (insT==0x75) {
+				programcounter++;
+			} else if (insT==0x85) {
+				programcounter++;
+			} else if (insT==0x06) {
+				programcounter++;
+			} else if (insT==0x16) {
+				programcounter++;
+			} else if (insT==0x26) {
+				programcounter++;
+			} else if (insT==0x36) {
+				programcounter++;
+			} else if (insT==0x46) {
+				programcounter++;
+			} else if (insT==0x56) {
+				newregisters[regX] = oldregisters[regY];
+				programcounter++;
+			} else if (insT==0x66) {
+				programcounter++;
+			} else if (insT==0x76) {
+				programcounter++;
+			} else if (insT==0x86) {
+				programcounter++;
+			} else if (insT==0x96) {
+				programcounter++;
+			} else if (insT==0xA6) {
+				programcounter++;
+			} else if (insT==0xB6) {
+				programcounter++;
+			} else if (insT==0xC6) {
+				programcounter++;
+			} else if (insT==0x07) {
+				programcounter++;
+			} else if (insT==0x17) {
+				programcounter++;
+			} else if (insT==0x27) {
+				programcounter++;
+			} else if (insT==0x37) {
+				programcounter++;
+			} else if (insT==0x47) {
+				programcounter++;
+			} else if (insT==0x57) {
+				programcounter++;
+			} else if (insT==0x67) {
+				programcounter++;
+			} else if (insT==0x77) {
+				programcounter++;
+			} else if (insT==0x87) {
+				programcounter++;
+			} else if (insT==0x97) {
 				programcounter++;
 			} else {
 				programcounter++;
