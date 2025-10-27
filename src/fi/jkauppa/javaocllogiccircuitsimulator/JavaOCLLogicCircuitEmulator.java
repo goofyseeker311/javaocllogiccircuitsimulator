51cdcbd96be161a1e702e2390d01ea70d2ce707a
goofyseeker311 <goofyseeker311@gmail.com>
2025-10-27 03:32:28 +0200

    Update JavaOCLLogicCircuitEmulator.java
    implemented all remaining instructions except multiply overflow which is only partially implemented detecting overflow. added first 16 registers debug output to instruction state/step output.

--- src/fi/jkauppa/javaocllogiccircuitsimulator/JavaOCLLogicCircuitEmulator.java
+++ src/fi/jkauppa/javaocllogiccircuitsimulator/JavaOCLLogicCircuitEmulator.java
@@ -73,6 +73,8 @@ public class JavaOCLLogicCircuitEmulator {
 		private int programcounter = 0;
 		private ByteBuffer instbytes = ByteBuffer.allocate(8);
 		private ByteBuffer longbytes = ByteBuffer.allocate(8);
+		private ByteBuffer longbytes2 = ByteBuffer.allocate(8);
+		private ByteBuffer longbytes3 = ByteBuffer.allocate(8);
 		
 		public void loadprogram(long[] program) {
 			if (program!=null) {
@@ -84,7 +86,12 @@ public class JavaOCLLogicCircuitEmulator {
 		
 		public void processinstruction() {
 			instructionstate = memoryram[programcounter];
-			System.out.println("instructionstate: "+String.format("%016x", instructionstate)+", instructionstep: "+instructionstep);
+			System.out.println("instructionstate: "+String.format("%016x", instructionstate)+", instructionstep: "+instructionstep+
+					", r0:"+String.format("%016x", oldregisters[0x0])+", r1:"+String.format("%016x", oldregisters[0x1])+", r2:"+String.format("%016x", oldregisters[0x2])+", r3:"+String.format("%016x", oldregisters[0x3])+
+					", r4:"+String.format("%016x", oldregisters[0x4])+", r5:"+String.format("%016x", oldregisters[0x5])+", r6:"+String.format("%016x", oldregisters[0x6])+", r7:"+String.format("%016x", oldregisters[0x7])+
+					", r8:"+String.format("%016x", oldregisters[0x8])+", r9:"+String.format("%016x", oldregisters[0x9])+", rA:"+String.format("%016x", oldregisters[0xA])+", rB:"+String.format("%016x", oldregisters[0xB])+
+					", rC:"+String.format("%016x", oldregisters[0xC])+", rD:"+String.format("%016x", oldregisters[0xD])+", rE:"+String.format("%016x", oldregisters[0xE])+", rF:"+String.format("%016x", oldregisters[0xF])
+					);
 			instbytes.clear();
 			instbytes.putLong(instructionstate).rewind();
 			int regX = instbytes.getShort();
@@ -162,68 +169,167 @@ public class JavaOCLLogicCircuitEmulator {
 				newregisters[regX] = oldregisters[regY] + oldregisters[regZ];
 				programcounter++;
 			} else if (insT==0x15) {
+				try {
+					Math.addExact(oldregisters[regY], oldregisters[regZ]);
+				} catch (ArithmeticException e) {
+					newregisters[regX] = oldregisters[regX]&(~(0x1<<bitI));
+					newregisters[regX] = newregisters[regX]|(0x1<<bitI);
+				}
 				programcounter++;
 			} else if (insT==0x25) {
 				newregisters[regX] = oldregisters[regY] - oldregisters[regZ];
 				programcounter++;
 			} else if (insT==0x35) {
+				try {
+					Math.subtractExact(oldregisters[regY], oldregisters[regZ]);
+				} catch (ArithmeticException e) {
+					newregisters[regX] = oldregisters[regX]&(~(0x1<<bitI));
+					newregisters[regX] = newregisters[regX]|(0x1<<bitI);
+				}
 				programcounter++;
 			} else if (insT==0x45) {
+				newregisters[regX] = oldregisters[regY] * oldregisters[regZ];
 				programcounter++;
 			} else if (insT==0x55) {
+				try {
+					Math.multiplyExact(oldregisters[regY], oldregisters[regZ]);
+				} catch (ArithmeticException e) {
+					// todo
+				}
 				programcounter++;
 			} else if (insT==0x65) {
+				newregisters[regX] = oldregisters[regY] / oldregisters[regZ];
 				programcounter++;
 			} else if (insT==0x75) {
+				newregisters[regX] = oldregisters[regY] % oldregisters[regZ];
 				programcounter++;
 			} else if (insT==0x85) {
+				newregisters[regX] = -oldregisters[regY];
 				programcounter++;
 			} else if (insT==0x06) {
+				newregisters[regX] = oldregisters[regY] << oldregisters[regZ];
 				programcounter++;
 			} else if (insT==0x16) {
+				newregisters[regX] = oldregisters[regY] >>> oldregisters[regZ];
 				programcounter++;
 			} else if (insT==0x26) {
+				newregisters[regX] = oldregisters[regY] >> oldregisters[regZ];
 				programcounter++;
 			} else if (insT==0x36) {
+				newregisters[regX] = Long.rotateLeft(oldregisters[regY], (int)oldregisters[regZ]);
 				programcounter++;
 			} else if (insT==0x46) {
+				newregisters[regX] = Long.rotateRight(oldregisters[regY], (int)oldregisters[regZ]);
 				programcounter++;
 			} else if (insT==0x56) {
 				newregisters[regX] = oldregisters[regY];
 				programcounter++;
 			} else if (insT==0x66) {
+				newregisters[regX] = ~oldregisters[regY];
 				programcounter++;
 			} else if (insT==0x76) {
+				newregisters[regX] = oldregisters[regY] | oldregisters[regZ];
 				programcounter++;
 			} else if (insT==0x86) {
+				newregisters[regX] = oldregisters[regY] & oldregisters[regZ];
 				programcounter++;
 			} else if (insT==0x96) {
+				newregisters[regX] = ~(oldregisters[regY] & oldregisters[regZ]);
 				programcounter++;
 			} else if (insT==0xA6) {
+				newregisters[regX] = ~(oldregisters[regY] | oldregisters[regZ]);
 				programcounter++;
 			} else if (insT==0xB6) {
+				newregisters[regX] = oldregisters[regY] ^ oldregisters[regZ];
 				programcounter++;
 			} else if (insT==0xC6) {
+				newregisters[regX] = ~(oldregisters[regY] ^ oldregisters[regZ]);
 				programcounter++;
 			} else if (insT==0x07) {
+				longbytes.clear();
+				longbytes.putLong(oldregisters[regY]).rewind();
+				double longdouble = longbytes.asDoubleBuffer().get();
+				longbytes2.clear();
+				longbytes2.putLong(oldregisters[regZ]).rewind();
+				double longdouble2 = longbytes2.asDoubleBuffer().get();
+				double longdouble3 = longdouble + longdouble2;
+				longbytes3.clear();
+				longbytes3.putDouble(longdouble3).rewind();
+				newregisters[regX] = longbytes3.getLong();
 				programcounter++;
 			} else if (insT==0x17) {
+				longbytes.clear();
+				longbytes.putLong(oldregisters[regY]).rewind();
+				double longdouble = longbytes.asDoubleBuffer().get();
+				longbytes2.clear();
+				longbytes2.putLong(oldregisters[regZ]).rewind();
+				double longdouble2 = longbytes2.asDoubleBuffer().get();
+				double longdouble3 = longdouble - longdouble2;
+				longbytes3.clear();
+				longbytes3.putDouble(longdouble3).rewind();
+				newregisters[regX] = longbytes3.getLong();
 				programcounter++;
 			} else if (insT==0x27) {
+				longbytes.clear();
+				longbytes.putLong(oldregisters[regY]).rewind();
+				double longdouble = longbytes.asDoubleBuffer().get();
+				longbytes2.clear();
+				longbytes2.putLong(oldregisters[regZ]).rewind();
+				double longdouble2 = longbytes2.asDoubleBuffer().get();
+				double longdouble3 = longdouble * longdouble2;
+				longbytes3.clear();
+				longbytes3.putDouble(longdouble3).rewind();
+				newregisters[regX] = longbytes3.getLong();
 				programcounter++;
 			} else if (insT==0x37) {
+				longbytes.clear();
+				longbytes.putLong(oldregisters[regY]).rewind();
+				double longdouble = longbytes.asDoubleBuffer().get();
+				longbytes2.clear();
+				longbytes2.putLong(oldregisters[regZ]).rewind();
+				double longdouble2 = longbytes2.asDoubleBuffer().get();
+				double longdouble3 = longdouble / longdouble2;
+				longbytes3.clear();
+				longbytes3.putDouble(longdouble3).rewind();
+				newregisters[regX] = longbytes3.getLong();
 				programcounter++;
 			} else if (insT==0x47) {
+				longbytes.clear();
+				longbytes.putLong(oldregisters[regY]).rewind();
+				double longdouble = longbytes.asDoubleBuffer().get();
+				double longdouble3 = -longdouble;
+				longbytes3.clear();
+				longbytes3.putDouble(longdouble3).rewind();
+				newregisters[regX] = longbytes3.getLong();
 				programcounter++;
 			} else if (insT==0x57) {
+				longbytes.clear();
+				longbytes.putDouble((double)oldregisters[regY]).rewind();
+				newregisters[regX] = longbytes.getLong();
 				programcounter++;
 			} else if (insT==0x67) {
+				longbytes.clear();
+				longbytes.putLong(oldregisters[regY]).rewind();
+				double longdouble = longbytes.asDoubleBuffer().get();
+				newregisters[regX] = Math.round(longdouble);
 				programcounter++;
 			} else if (insT==0x77) {
+				longbytes.clear();
+				longbytes.putLong(oldregisters[regY]).rewind();
+				double longdouble = longbytes.asDoubleBuffer().get();
+				newregisters[regX] = (long)Math.floor(longdouble);
 				programcounter++;
 			} else if (insT==0x87) {
+				longbytes.clear();
+				longbytes.putLong(oldregisters[regY]).rewind();
+				double longdouble = longbytes.asDoubleBuffer().get();
+				newregisters[regX] = (long)Math.ceil(longdouble);
 				programcounter++;
 			} else if (insT==0x97) {
+				longbytes.clear();
+				longbytes.putLong(oldregisters[regY]).rewind();
+				double longdouble = longbytes.asDoubleBuffer().get();
+				newregisters[regX] = (long)longdouble;
 				programcounter++;
 			} else {
 				programcounter++;
