f62b9ccc6615afcb25f9ec3498dad2446a74a7d3
goofyseeker311 <goofyseeker311@gmail.com>
2025-11-01 12:29:26 +0200

    update muxrisccore.circ
    changed program counter and local rom/ram/disp/nvram addressing from 18-bit to 26-bit, each of the memory type blocks having their own 24-bit addressing block with unique page high 2-bit address. added separate disp and nvram writable blocks per core. disp ram block is meant to directly display on native on-chip distributed led display at 64-bit hdr color 16-bits color components. nvram ram block is meant to be larger separate non-volatile bootable distributed nand for loading default boot programs if a boot rom is not connected. updated loader.asm and source.asm to work on the new local core memory addressing.

--- loader.asm
+++ loader.asm
@@ -1,7 +1,7 @@
-ldi  0000 00000000   // rom read index 0x00000 [ init variables ]
-ldi  0001 00020000   // ram write index 0x20000
+ldi  0000 00000000   // rom read index 0x0000000 [ init variables ]
+ldi  0001 01000000   // ram write index 0x1000000
 ldi  0002 00000001   // constant 0x1
-ldi  0003 00020000   // jump address 0x20000
+ldi  0003 01000000   // jump address 0x1000000
 ldi  0004 00000100   // rom to ram copy size
 ldi  0005 0000001A   // zero branch jump address
 ldi  0006 00000011   // non-zero branch jump address
